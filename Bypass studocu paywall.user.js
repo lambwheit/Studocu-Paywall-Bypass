2448bfd8bd0765aa3218d1220341bc0e0ce96a69
lambwheit <38606611+lambwheit@users.noreply.github.com>
2022-02-01 18:12:11 +0800

    add triple click to extract document

--- Bypass studocu paywall.user.js
+++ Bypass studocu paywall.user.js
@@ -24,6 +24,29 @@ function addGlobalStyle(css) {
     console.log("blur remover loaded")
     window.addEventListener('load', function() {
         console.log("load listener injected")
+        try{
+        window.addEventListener('click', function(evt){
+		if(evt.detail === 3){
+
+			var head = document.getElementsByTagName("head")[0].innerHTML;
+			var tit = document.getElementsByTagName("h1")[0].innerHTML;
+			var pages = document.getElementById('page-container').childNodes;
+
+			for(var i=0; i<pages.length; i++){
+				pages[i].childNodes[0].style = "display: block;";
+			}
+
+			var pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
+			var newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");
+			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print  {@page {size: A5;}}</style>";
+			newWindow.document.title = tit;
+			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
+			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
+		}
+	});}
+        catch(error){
+            console.log("failed to create doc viewer");
+        }
         setTimeout(() => {
         try {
             var element = document.getElementById('viewer-wrapper')
