a73006054c3e326278184ebfcf31089284634f89
lambwheit <38606611+lambwheit@users.noreply.github.com>
2022-05-07 23:21:42 +1000

    Update Bypass studocu paywall.user.js
    added ugly fix to remove overpage ads

--- Bypass studocu paywall.user.js
+++ Bypass studocu paywall.user.js
@@ -10,76 +10,84 @@
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
+    if (!head) {
+        return;
+    }
     style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
     head.appendChild(style);
 }
 
-(function()
-{
+(function() {
     'use strict';
     addGlobalStyle('.page-content{filter:blur(0px)!important}');
     console.log("blur remover loaded")
     window.addEventListener('load', function() {
         console.log("load listener injected")
-        try{
-        window.addEventListener('click', function(evt){
-		if(evt.detail === 3){
+        try {
+            window.addEventListener('click', function(evt) {
+                if (evt.detail === 3) {
 
-			var head = document.getElementsByTagName("head")[0].innerHTML;
-			var tit = document.getElementsByTagName("h1")[0].innerHTML;
-			var pages = document.getElementById('page-container').childNodes;
+                    var head = document.getElementsByTagName("head")[0].innerHTML;
+                    var tit = document.getElementsByTagName("h1")[0].innerHTML;
+                    var pages = document.getElementById('page-container').childNodes;
 
-			for(var i=0; i<pages.length; i++){
-				pages[i].childNodes[0].style = "display: block;";
-			}
+                    for (var i = 0; i < pages.length; i++) {
+                        pages[i].childNodes[0].style = "display: block;";
+                    }
 
-			var pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
-			var newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");
-			newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>";
-			newWindow.document.title = tit;
-			newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
-			newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
-		}
-	});}
-        catch(error){
+                    var pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
+                    var newWindow = window.open("", "Document", "height=865,width=625,status=yes,toolbar=no,menubar=no");
+                    newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>";
+                    newWindow.document.title = tit;
+                    newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
+                    newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
+                }
+            });
+        } catch (error) {
             console.log("failed to create doc viewer");
         }
         setTimeout(() => {
-        try {
-            var element = document.getElementById('viewer-wrapper')
-            if(element != null){
-                var elements = element.childNodes;
-                if (elements.length>3){
-                    elements[3].parentNode.removeChild(elements[3]);
-                }
-            }
-        } catch (error) {
-            console.log("no over-page ads found")
-        }
-        try{
-            var pages = document.getElementsByClassName('page-content');
-            for(var i=0; i<pages.length; i++)
-            {
-                var pagecontent=pages[i].parentNode.childNodes;
-                for(var j=0; j<pagecontent.length; j++)
-                {
-                    if(pagecontent[j].className != "page-content")
-                    {
-                        pagecontent[j].parentNode.removeChild(pagecontent[j]);
+            try {
+                var element = document.getElementById('viewer-wrapper')
+                if (element != null) {
+                    var elements = element.childNodes;
+                    if (elements.length > 3) {
+                        elements[3].parentNode.removeChild(elements[3]);
                     }
                 }
+            } catch (error) {
+                console.log("no over-page ads found")
             }
-        }
-        catch(error)
-        {
-            console.log("no ads on top of pages found")
-        }
-    }, 100);
+            try {
+                var pages = document.getElementsByClassName('page-content');
+                for (var i = 0; i < pages.length; i++) {
+                    var pagecontent = pages[i].parentNode.childNodes;
+                    for (var j = 0; j < pagecontent.length; j++) {
+                        if (pagecontent[j].className != "page-content") {
+                            pagecontent[j].parentNode.removeChild(pagecontent[j]);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.log("no ads on top of pages found")
+            }
+            try {
+                var ad = document.getElementsByClassName("_9b5d16e08bb1")
+                for (var i = 0; i < ad.length; i++) {
+                    var pagecontent = ad[i].parentNode.childNodes;
+                    for (var j = 0; j < pagecontent.length; j++) {
+                        pagecontent[j].parentNode.removeChild(pagecontent[j]);
 
-})
+                    }
+                }
+            } catch (errror) {
+                console.log("no overpage ads found")
+            }
+        }, 100);
+
+    })
 
 
 })();
