82034b1d7ef86b7247003ca90b990fc5c6e8b10f
lambwheit <38606611+lambwheit@users.noreply.github.com>
2023-01-24 10:38:18 +0800

    qwe
    qwe

--- Bypass studocu paywall.user.js
+++ Bypass studocu paywall.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bypass studocu paywall
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.25
 // @description  try to take over the world!
 // @author       https://github.com/lambwheit
 // @match        https://studeersnel.nl/*
